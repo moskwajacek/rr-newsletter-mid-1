211437421bd7ee103647b46731b4eb2722b6f446
Jacek Moskwa <moskwajacek@gmail.com>
2018-07-04 17:13:57 +0200

    task ready without comments

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.sayHello3 = this.sayHello3.bind(this);
+  }
+
   sayHello() {
     // NOTE this implementation may be quite large and needs to stay in a separate function
+    alert(`Hello from ${this.props.name}!`);
+  }
+
+  sayHello2 = () => {
+    alert(`Hello from ${this.props.name}!`);
+  }
+
+  sayHello3 () {
     alert(`Hello from ${this.props.name}!`);
   }
 
@@ -18,9 +31,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={this.sayHello}>Say Hello #1</button>
-        <button>Say Hello #2</button>
-        <button>Say Hello #3</button>
+        <button onClick={this.sayHello.bind(this)}>Say Hello #1</button>
+        <button onClick={this.sayHello2}>Say Hello #2</button>
+        <button onClick={this.sayHello3}>Say Hello #3</button>
       </div>
     );
   }
